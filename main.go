c2d62b03f73d98e9bc3c4672bb2e2f3f9715a5a6
ManiacZX <maniaczx@gmail.com>
2017-11-16 23:12:42 -0600

    added network monitor

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"os/exec"
 	"time"
@@ -10,7 +11,12 @@ import (
 func main() {
 	started := time.Now()
 
-	wvdial()
+	terminate := make(chan string)
+
+	go wvdial(terminate)
+	go netmon(terminate)
+
+	<-terminate
 
 	for {
 		if time.Since(started) > 5*time.Minute {
@@ -22,11 +28,28 @@ func main() {
 	reboot()
 }
 
-func wvdial() {
+func wvdial(terminate chan<- string) {
 	cmd := exec.Command("wvdial")
 	err := cmd.Run()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error executing wvdial: %v", err)
+	}
+	terminate <- "wvdial"
+}
+
+func netmon(terminate chan<- string) {
+	errCount := 0
+	for {
+		_, err := http.Get("https://www.google.com")
+		if err != nil {
+			errCount++
+			if errCount > 5 {
+				terminate <- "netmon"
+				break
+			}
+		} else {
+			errCount = 0
+		}
 	}
 }
 
